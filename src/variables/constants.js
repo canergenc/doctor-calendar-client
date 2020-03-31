9cbd533e8b95d284b01269fa69446204061db45c
gokhanku <gokhan.kurt@outlook.com>
2020-03-31 12:19:15 +0300

    Added CalendarType

--- src/variables/constants.js
+++ src/variables/constants.js
@@ -15,3 +15,16 @@ export const constants = {
 
     GROUPID:'groupId'
 };
+
+
+export const CalendarType = {
+    "Nöbet": 1,
+    "İzin": 2,
+    "Rapor": 3,
+    "Gebelik": 4,
+    "Resmi Tatil": 5,
+    "İdari izin": 6,
+    "Özel Durum": 7,
+}
+
+
